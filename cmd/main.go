e0cd7636f209ecb24dd5295eeef892a5e69aac85
AbdulrahmanDaud10 <dulisyke3015@gmail.com>
2023-05-20 23:50:07 +0300

    server addition

---
+++ cmd/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	server := gin.Default()
+	server.GET("/api/healthchecker", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{"status": "success", "message": "Implement Google 0Auth2 in Golang"})
+	})
+
+	log.Fatal(server.Run(":" + "8080"))
+}
